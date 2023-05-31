c2545a98e72552ab5fcd38d1762f42f3523a645e
Praveen Vamsi <praveenvamsi8874@gmail.com>
2023-05-31 21:08:54 +0530

    CRUD Operations

--- tennis-player/src/main/java/io/datajek/sprindatajpa/PlayerRepository.java
+++ tennis-player/src/main/java/io/datajek/sprindatajpa/PlayerRepository.java
@@ -11,4 +11,21 @@ import javax.persistence.PersistenceContext;
 public class PlayerRepository {
     @PersistenceContext
     EntityManager entityManager;
+
+    public Player insertPlayer(Player player) {
+        return entityManager.merge(player);
+    }
+
+    public Player updatePlayer(Player player) {
+        return entityManager.merge(player);
+    }
+
+    public Player getPlayerById(int id) {
+        return entityManager.find(Player.class, id);
+    }
+
+    public void deleteById(int id) {
+        Player player = entityManager.find(Player.class, id);
+        entityManager.remove(player);
+    }
 }
--- tennis-player/src/main/java/io/datajek/sprindatajpa/TennisPlayerApplication.java
+++ tennis-player/src/main/java/io/datajek/sprindatajpa/TennisPlayerApplication.java
@@ -8,9 +8,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springdata.tennisplayer.PlayerDao;
 
+import java.sql.Date;
+
 @SpringBootApplication
-public class TennisPlayerApplication {
+public class TennisPlayerApplication implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PlayerRepository repo;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TennisPlayerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("\n\n>> Inserting Player: {}\n", repo.insertPlayer(
+				new Player("Djokovic", "Serbia", Date.valueOf("1987-05-22"), 81)
+		));
+
+		logger.info("\n\n>> Inserting Player: {}\n", repo.insertPlayer(
+				new Player("Monfils", "France", Date.valueOf("1986-09-01"), 10)
+		));
+
+		logger.info("\n\n>> Inserting Player: {}\n", repo.insertPlayer(
+				new Player("Thiem", "Austria",
+						new Date(System.currentTimeMillis()), 17)));
+
+//		logger.info("\n\n>> Player with id 2: {}\n", repo.getPlayerById(2));
+
+		//update player
+		logger.info("\n\n>> Updating Player with Id 3: {}\n", repo.updatePlayer(
+				new Player(3, "Thiem", "Austria", Date.valueOf("1993-09-03"), 17)));
+		//get player
+		logger.info("\n\n>> Player with id 3: {}\n", repo.getPlayerById(3));
+
+		repo.deleteById(2);
 	}
 }

0638f168a77769d88619cac10d581d9a7be8ce89
Praveen Vamsi <praveenvamsi8874@gmail.com>
2023-06-01 11:44:35 +0530

    corrections

--- tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerController.java
+++ tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerController.java
@@ -1,7 +1,12 @@
 package io.datajek.tennisplayerrest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlayerController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Tennis Player REST API";
+    }
 }
--- tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/TennisPlayerRestApplication.java
+++ tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/TennisPlayerRestApplication.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TennisPlayerRestApplication {
-	@GetMapping("/welcome")
-	public String welcome() {
-		return "Tennis Player REST API";
-	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(TennisPlayerRestApplication.class, args);
 	}

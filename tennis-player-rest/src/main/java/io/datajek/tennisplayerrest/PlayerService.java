2658d270d23277ed8d98b22199d8b3b1ed51c183
Praveen Vamsi <praveenvamsi8874@gmail.com>
2023-06-01 11:51:43 +0530

    @GetMapping

--- tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerService.java
+++ tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/PlayerService.java
@@ -3,8 +3,14 @@ package io.datajek.tennisplayerrest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PlayerService {
     @Autowired
     private PlayerRepository repo;
+
+    public List<Player> getAllPlayers() {
+        return repo.findAll();
+    }
 }

556a95e541c97f905d58873a6ec144cdeaf38a0e
Praveen Vamsi <praveenvamsi8874@gmail.com>
2023-06-01 11:42:08 +0530

    Creating aa REST Controller

--- tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/TennisPlayerRestApplication.java
+++ tennis-player-rest/src/main/java/io/datajek/tennisplayerrest/TennisPlayerRestApplication.java
@@ -2,10 +2,14 @@ package io.datajek.tennisplayerrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TennisPlayerRestApplication {
-
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Tennis Player REST API";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TennisPlayerRestApplication.class, args);
 	}
